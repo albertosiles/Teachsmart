5ccbaf67a33ffc5e68bc02bb43e873c5ebca0d7c
Theresa Niedernhuber <theresa-niedernhuber@live.de>
2024-03-02 03:32:20 +0100

    resource crud fixed

--- db/migrate/20240302162829_add_user_to_resources.rb
+++ db/migrate/20240302162829_add_user_to_resources.rb
@@ -1,4 +1,4 @@
-class AddUserResources < ActiveRecord::Migration[7.1]
+class AddUserToResources < ActiveRecord::Migration[7.1]
   def change
     add_reference :resources, :user, null: false, foreign_key: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_02_162828) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_02_162829) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,7 +57,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_02_162828) do
     t.string "resource_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_resources_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -84,5 +85,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_02_162828) do
   add_foreign_key "bookmarks", "resources"
   add_foreign_key "forum_posts", "forum_threads"
   add_foreign_key "forum_threads", "users"
+  add_foreign_key "resources", "users"
   add_foreign_key "reviews", "resources"
 end
