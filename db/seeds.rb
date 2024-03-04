0340fd875b3a5e1d00cd011df9e6f4c37f7859f0
Jeremy Thorpe <tpyth0n@hotmail.com>
2024-03-04 16:48:14 +0000

    Created seeds for resources and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "seeding"
+
+Resource.destroy_all
+User.destroy_all
+
+Resource.create(title: "Basic Algebra", description: "Introduction to simple equations", subject: "Mathematics", student_age: 11, grade: 5, category: "practice", resource_type: "PDF")
+Resource.create(title: "Past Tense", description: "Past tenses in German", subject: "German", student_age: 12, grade: 6, category: "practice", resource_type: "PDF")
+Resource.create(title: "States", description: "Map of USA showing states", subject: "Geography", student_age: 14, grade: 8, category: "presentation", resource_type: "PowerPoint")
+Resource.create(title: "Teacher Presentation Pack", description: "Great resource for physics teachers", subject: "Physics", student_age: 14, grade: 8, category: "presentation", resource_type: "PowerPoint")
+Resource.create(title: "Writing Formal Letters", description: "Introduction to using formal style in letter writing", subject: "English", student_age: 15, grade: 9, category: "practice", resource_type: "PDF")
+Resource.create(title: "Fractions and Decimals", description: "Lots of practice exercises", subject: "Mathematics", student_age: 12, grade: 6, category: "practice", resource_type: "PDF")
+
+user_Julia = User.create!(email: "julia@lewagon.com", password: "xxx111")
+user_Robert = User.create!(email: "robert@lewagon.com", password: "xxx222")
+user_Bridget = User.create!(email: "bridget@lewagon.com", password: "xxx333")
