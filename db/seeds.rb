03e3a6d0349d3a97b69acfdb038222f2b956d6ae
Jeremy Thorpe <tpyth0n@hotmail.com>
2024-03-07 15:57:15 +0000

    deleted grade in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,12 +12,12 @@ puts "seeding"
 Resource.destroy_all
 User.destroy_all
 
-Resource.create(title: "Basic Algebra", description: "Introduction to simple equations", subject: "Mathematics", student_age: 11, grade: 5, category: "practice", resource_type: "PDF")
-Resource.create(title: "Past Tense", description: "Past tenses in German", subject: "German", student_age: 12, grade: 6, category: "practice", resource_type: "PDF")
-Resource.create(title: "States", description: "Map of USA showing states", subject: "Geography", student_age: 14, grade: 8, category: "presentation", resource_type: "PowerPoint")
-Resource.create(title: "Teacher Presentation Pack", description: "Great resource for physics teachers", subject: "Physics", student_age: 14, grade: 8, category: "presentation", resource_type: "PowerPoint")
-Resource.create(title: "Writing Formal Letters", description: "Introduction to using formal style in letter writing", subject: "English", student_age: 15, grade: 9, category: "practice", resource_type: "PDF")
-Resource.create(title: "Fractions and Decimals", description: "Lots of practice exercises", subject: "Mathematics", student_age: 12, grade: 6, category: "practice", resource_type: "PDF")
+Resource.create(title: "Basic Algebra", description: "Introduction to simple equations", subject: "Mathematics", student_age: 11, category: "practice", resource_type: "PDF")
+Resource.create(title: "Past Tense", description: "Past tenses in German", subject: "German", student_age: 12, category: "practice", resource_type: "PDF")
+Resource.create(title: "States", description: "Map of USA showing states", subject: "Geography", student_age: 14, category: "presentation", resource_type: "PowerPoint")
+Resource.create(title: "Teacher Presentation Pack", description: "Great resource for physics teachers", subject: "Physics", student_age: 14, category: "presentation", resource_type: "PowerPoint")
+Resource.create(title: "Writing Formal Letters", description: "Introduction to using formal style in letter writing", subject: "English", student_age: 15, category: "practice", resource_type: "PDF")
+Resource.create(title: "Fractions and Decimals", description: "Lots of practice exercises", subject: "Mathematics", student_age: 12, category: "practice", resource_type: "PDF")
 
 user_Julia = User.create!(email: "julia@lewagon.com", password: "xxx111")
 user_Robert = User.create!(email: "robert@lewagon.com", password: "xxx222")
