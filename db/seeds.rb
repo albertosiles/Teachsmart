c2df92703486a99391cc6e622b4bb79a11667862
Jeremy Thorpe <tpyth0n@hotmail.com>
2024-03-07 18:20:55 +0000

    made minor changes to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-puts "seeding"
-
-Resource.destroy_all
+puts "Cleaning database..."
 User.destroy_all
-
-Resource.create(title: "Basic Algebra", description: "Introduction to simple equations", subject: "Mathematics", student_age: 11, category: "practice", resource_type: "PDF")
-Resource.create(title: "Past Tense", description: "Past tenses in German", subject: "German", student_age: 12, category: "practice", resource_type: "PDF")
-Resource.create(title: "States", description: "Map of USA showing states", subject: "Geography", student_age: 14, category: "presentation", resource_type: "PowerPoint")
-Resource.create(title: "Teacher Presentation Pack", description: "Great resource for physics teachers", subject: "Physics", student_age: 14, category: "presentation", resource_type: "PowerPoint")
-Resource.create(title: "Writing Formal Letters", description: "Introduction to using formal style in letter writing", subject: "English", student_age: 15, category: "practice", resource_type: "PDF")
-Resource.create(title: "Fractions and Decimals", description: "Lots of practice exercises", subject: "Mathematics", student_age: 12, category: "practice", resource_type: "PDF")
+puts "Creating users..."
+puts "seeding..."
 
 user_Julia = User.create!(email: "julia@lewagon.com", password: "xxx111")
 user_Robert = User.create!(email: "robert@lewagon.com", password: "xxx222")
 user_Bridget = User.create!(email: "bridget@lewagon.com", password: "xxx333")
+
+Resource.destroy_all
+puts "Creating resources..."
+puts "seeding"
+
+resource_basic_algebra = Resource.create!(title: "Basic Algebra", description: "Introduction to simple equations", subject: "Mathematics", student_age: 11, category: "practice", resource_type: "PDF")
+reource_past_tense = Resource.create!(title: "Past Tense", description: "Past tenses in German", subject: "German", student_age: 12, category: "practice", resource_type: "PDF")
+resource_states = Resource.create!(title: "States", description: "Map of USA showing states", subject: "Geography", student_age: 14, category: "presentation", resource_type: "PowerPoint")
+resource_presentation = Resource.create!(title: "Teacher Presentation Pack", description: "Great resource for physics teachers", subject: "Physics", student_age: 14, category: "presentation", resource_type: "PowerPoint")
+resource_formal_letters = Resource.create!(title: "Writing Formal Letters", description: "Introduction to using formal style in letter writing", subject: "English", student_age: 15, category: "practice", resource_type: "PDF")
+resource_fractions_and_decimals = Resource.create!(title: "Fractions and Decimals", description: "Lots of practice exercises", subject: "Mathematics", student_age: 12, category: "practice", resource_type: "PDF")
