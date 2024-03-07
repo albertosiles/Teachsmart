ea4538514672f4a0d70c604efd3a3fab1465f183
Theresa Niedernhuber <theresa-niedernhuber@live.de>
2024-03-07 23:21:59 +0100

    adjustment routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Resources routes
-  resources :resources
+  resources :resources do
   resources :reviews, only: [:new, :create]
+  end
 
   resources :reviews, only: [:destroy]
 
