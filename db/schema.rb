fcda06d2775db03f909e1af30f4452b154a48d8e
Pablo Fanjul <pfanjul81@gmail.com>
2024-03-19 11:25:57 +0100

    db rollback

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_19_095437) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_15_103712) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -89,7 +89,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_19_095437) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.string "cloudinary_photo_key"
     t.index ["user_id"], name: "index_resources_on_user_id"
   end
 
